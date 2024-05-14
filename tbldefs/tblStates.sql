522cef2c8a0d8ea45be605c112e37c5f55fa142f
srajkumar0608 <108138834+srajkumar0608@users.noreply.github.com>
2024-05-14 16:20:50 +0530

    Add files via upload

---
+++ tbldefs/tblStates.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [tblStates] (
+  [StateID] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
+  [StateName] VARCHAR (255),
+  [FileName] VARCHAR (255),
+  [FileName_Claborn] VARCHAR (255)
+)
---
+++ tbldefs/tblStates_Email.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [tblStates_Email] (
+  [StateEmailIDs] AUTOINCREMENT CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
+  [State] LONG CONSTRAINT [State] UNIQUE,
+  [ToEmailAddress] VARCHAR (255),
+  [CCEmailAddress] VARCHAR (255),
+  [EmailFrom] VARCHAR (255),
+  [Subject] VARCHAR (255),
+  [NameForEmail] VARCHAR (255)
+)
